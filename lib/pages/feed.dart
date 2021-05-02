f9993b925a46360e3b5b6df28176801b36c9d3ec
aryanj01 <aryanjasra01@gmail.com>
2021-05-02 07:55:25 +0530

    images

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -4,7 +4,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:team_undefined/pages/app_drawer.dart';
 
-
+enum WidgetMarker{
+  missing,found,extra
+}
 class Explore extends StatefulWidget {
   @override
   _ExploreState createState() => _ExploreState();
@@ -12,8 +14,66 @@ class Explore extends StatefulWidget {
 
 class _ExploreState extends State<Explore> {
   int _selectedItemIndex = 0;
+  WidgetMarker selectedwidgetmarker=WidgetMarker.missing;
   List<bool> isSelected = [false , false];
   @override
+  Widget getcustomgridleview(){
+    switch(selectedwidgetmarker){
+      case WidgetMarker.missing:
+        return getmissingview();
+      case WidgetMarker.found:
+        return getfoundmissingview();
+      case WidgetMarker.extra:
+        return getmissingview();
+    }
+    return getcustomgridleview();
+  }
+  Widget getmissingview(){
+    return GridView.count(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      crossAxisCount: 3,
+      crossAxisSpacing: 5,
+      mainAxisSpacing: 10,
+      children: [
+
+        buildPictureCard('https://images.unsplash.com/photo-1595064585256-007759ad2316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1619451831105-599f9b2b94f3?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1615942581207-42a20ca5cd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1598527661665-af197ec31685?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1862&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1614398077768-f6869e7a8137?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1588701107566-af76b932e2e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1610919207284-983b309c8469?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1611903708839-31b28b003935?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1615942581207-42a20ca5cd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1578923813875-bf8b98f36b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzB8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1616286949780-50d29ce2ae63?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1544159729-c9380464148c?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDY1fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+      ],
+    );
+  }
+  Widget getfoundmissingview(){
+    return GridView.count(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      crossAxisCount: 3,
+      crossAxisSpacing: 5,
+      mainAxisSpacing: 10,
+      children: [
+        buildPictureCard('https://images.unsplash.com/photo-1614398077768-f6869e7a8137?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1588701107566-af76b932e2e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1610919207284-983b309c8469?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1611903708839-31b28b003935?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1615942581207-42a20ca5cd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1578923813875-bf8b98f36b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzB8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1616286949780-50d29ce2ae63?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1544159729-c9380464148c?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDY1fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1595064585256-007759ad2316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1619451831105-599f9b2b94f3?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        buildPictureCard('https://images.unsplash.com/photo-1615942581207-42a20ca5cd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'),
+        buildPictureCard('https://images.unsplash.com/photo-1598527661665-af197ec31685?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1862&q=80'),
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -34,7 +94,7 @@ class _ExploreState extends State<Explore> {
           //mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(0.0,15.0,0.0,0.0),
+              padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
               child: Text(
                 'Explore Feed',
                 style: TextStyle(
@@ -53,7 +113,7 @@ class _ExploreState extends State<Explore> {
               ),
               child: ToggleButtons(
                 borderRadius: BorderRadius.circular(30.0),
-                isSelected:isSelected,
+                isSelected: isSelected,
                 //renderBorder: true,
                 color: Colors.black,
                 fillColor: Colors.white,
@@ -62,9 +122,9 @@ class _ExploreState extends State<Explore> {
                 borderColor: Colors.grey[500],
                 selectedBorderColor: Colors.grey[500],
 
-                children:<Widget> [
+                children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(70.0,7.0,70.0,7.0),
+                    padding: const EdgeInsets.fromLTRB(70.0, 7.0, 70.0, 7.0),
                     child: Text(
                       'MISSING',
                       style: TextStyle(
@@ -74,7 +134,7 @@ class _ExploreState extends State<Explore> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(70.0,7.0,70.0,7.0),
+                    padding: const EdgeInsets.fromLTRB(70.0, 7.0, 70.0, 7.0),
                     child: Text(
                       'FOUND',
                       style: TextStyle(
@@ -85,15 +145,21 @@ class _ExploreState extends State<Explore> {
                   ),
 
                 ],
-                onPressed:(index) {
+                onPressed: (index) {
                   setState(() {
-                    for(var i=0;i<isSelected.length;i++){
-                      if(i==index){
-                        isSelected[i]=true;
+                    for (var i = 0; i < isSelected.length; i++) {
+                      if (i == index) {
+                        isSelected[i] = true;
                       }
-                      else{
-                        isSelected[i]=false;
+                      else {
+                        isSelected[i] = false;
                       }
+                    }
+                    if (index == 0) {
+                      selectedwidgetmarker = WidgetMarker.missing;
+                    }
+                    else {
+                      selectedwidgetmarker = WidgetMarker.found;
                     }
                   });
                 },
@@ -105,29 +171,10 @@ class _ExploreState extends State<Explore> {
               child: Container(
                 margin: EdgeInsets.only(left: 0, right: 0, top: 8),
                 decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.15),
-                    ),
-
-                child: GridView.count(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                  crossAxisCount: 3,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 10,
-                children: [
-                  buildPictureCard('https://images.unsplash.com/photo-1614398077768-f6869e7a8137?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80'),
-                  buildPictureCard('https://images.unsplash.com/photo-1588701107566-af76b932e2e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
-                  buildPictureCard('https://images.unsplash.com/photo-1610919207284-983b309c8469?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
-                  buildPictureCard('https://images.unsplash.com/photo-1611903708839-31b28b003935?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
-                  buildPictureCard('https://images.unsplash.com/photo-1615942581207-42a20ca5cd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'),
-                  buildPictureCard('https://images.unsplash.com/photo-1578923813875-bf8b98f36b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzB8fGNoaWxkJTIwcG90cmFpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
-                  buildPictureCard('https://images.unsplash.com/photo-1616286949780-50d29ce2ae63?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
-                  buildPictureCard('https://images.unsplash.com/photo-1544159729-c9380464148c?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDY1fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
-                  buildPictureCard('https://images.unsplash.com/photo-1595064585256-007759ad2316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80'),
-                  buildPictureCard('https://images.unsplash.com/photo-1619451831105-599f9b2b94f3?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
-                  buildPictureCard('https://images.unsplash.com/photo-1615942581207-42a20ca5cd5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80'),
-                  buildPictureCard('https://images.unsplash.com/photo-1598527661665-af197ec31685?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1862&q=80'),
-                ],
+                  color: Colors.grey.withOpacity(0.15),
                 ),
+
+                child: getcustomgridleview(),
               ),
             ),
           ],
@@ -138,8 +185,8 @@ class _ExploreState extends State<Explore> {
         height: 60,
         child: Container(
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(100),
+            color: Colors.grey[200],
           ),
           child:
           Padding(
@@ -175,6 +222,7 @@ class _ExploreState extends State<Explore> {
       ),
     );
   }
+
   Widget buildNavBarItem(IconData icon, int index) {
     return GestureDetector(
       onTap: () {
