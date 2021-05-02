f877835ce554d0b1707f17c951f27776d5fc0635
Sushila Kaur <56120047+sushilakaur@users.noreply.github.com>
2021-05-02 07:03:03 +0530

    Pop up card added

---
+++ lib/utils/authentication.dart
@@ -0,0 +1,11 @@
+import 'package:'
+
+class Authentication {
+  static Future<FirebaseApp> initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+
+    // TODO: Add auto login logic
+
+    return firebaseApp;
+  }
+}
